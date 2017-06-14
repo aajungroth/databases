7839c59751b2d5f4fa0b5354600610d841268d05
Alex Jungroth <aajungroth@ucdavis.edu>
2017-06-14 03:18:24 +0000

    Connect sever to the databaase with index.js db

--- server/db/index.js
+++ server/db/index.js
@@ -3,3 +3,19 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var dbConnection = mysql.createConnection({
+  user: 'root',
+  password: '',
+  database: 'chat'
+});
+
+dbConnection.on('connect', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('CONNECTED SON!!');
+    exports.dbConnection;
+  }
+});
+
+dbConnection.connect();
